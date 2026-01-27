e1862b47b8a059a71eab074210f0a38b1559836a
Vamsi Krishna <vamsitvk225@gmail.com>
2026-01-27 19:33:03 +0530

    Setup GitHub Pages deployment with Actions

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,7 @@ export default defineConfig(({ mode }) => {
         alias: {
           '@': path.resolve(__dirname, '.'),
         }
-      }
+      },
+      base: '/POS_System_Sales_Page/',
     };
 });
