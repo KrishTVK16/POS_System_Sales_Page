9428adafc8fb4e43ac4426af737f3623ef8079b6
Vamsi Krishna <vamsitvk225@gmail.com>
2026-01-27 20:06:04 +0530

    fix1

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./index.html",
+        "./components/**/*.{js,ts,jsx,tsx}",
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./src/**/*.{js,ts,jsx,tsx}",
+        "./*.{js,ts,jsx,tsx}"
+    ],
+    darkMode: 'class',
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Inter', 'sans-serif'],
+                outfit: ['Outfit', 'sans-serif'],
+            },
+        },
+    },
+    plugins: [],
+}
